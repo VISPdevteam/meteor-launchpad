1121fc8c70d20dc9e089ace4bae903bb2f141019
Jameson Vallar <jvallarvisp@users.noreply.github.com>
2017-04-29 10:08:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker build -t jvallarvisp/meteor-launchpad:base .
-docker build -f dev.dockerfile -t jvallarvisp/meteor-launchpad:devbuild .
-docker build -f prod.dockerfile -t jvallarvisp/meteor-launchpad:latest .
+docker build -t vispdevteam/meteor-launchpad:base .
+docker build -f dev.dockerfile -t vispdevteam/meteor-launchpad:devbuild .
+docker build -f prod.dockerfile -t vispdevteam/meteor-launchpad:latest .
